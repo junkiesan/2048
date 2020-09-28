b076092ef34321417a8afc55e8f2d4dd6a4289fb
Julien Bouland <bouland.julien@gmail.com>
2020-09-28 18:28:55 +0200

    first commit

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import './App.css';
 
 function App() {
   return (
-    <div className="2048">
+    <div className="2048 Game">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
