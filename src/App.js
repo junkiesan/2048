817e136d822a13ce78fe330b47baec1aece1fd40
Julien Bouland <bouland.julien@gmail.com>
2020-09-29 21:43:07 +0200

    Added basic style

--- src/App.js
+++ src/App.js
@@ -17,12 +17,61 @@ function App() {
   // Check Gameover
 
   // Reset Game
-  
-  return {data.map(singleRow=>{
 
-  })};
+  return (
+    <div
+      style={{
+        background: "#AD9D8F",
+        width: "max-content",
+        height: "max-content",
+        margin: "auto",
+        padding: 5,
+        borderRadius: 5,
+        marginTop: 10,
+        position: "relative",
+      }}
+    >
+        {data.map((row, oneIndex) => {
+          return (
+            <div style={{ display: "flex" }} key={oneIndex}>
+              {row.map((digit, index) => (
+                <Block num={digit} key={index} />
+              ))}
+            </div>
+          );
+        })}
+    </div>
+  );
 }
+  const Block = ({ num }) => {
+    const { blockStyle } = style;
 
+    return (
+      <div
+        style={{
+          ...blockStyle,
+          // background: getColors(num),
+          color: num === 2 || num === 4 ? "#645B52" : "#F7F4EF",
+        }}
+      >
+        { num }
+        {/* {num !== 0 ? num : ""} */}
+      </div>
+    );
+  };
+
+  const style = {
+    blockStyle: {
+      height: 80,
+      width: 80,
+      background: "lightgray",
+      margin: 3,
+      display: "flex",
+      justifyContent: "center",
+      alignItems: "center",
+      fontSize: 45,
+      fontWeight: "800",
+      color: "white",
+    },
+  };
 export default App;
-
-<div>{JSON.stringify(data)}</div>
