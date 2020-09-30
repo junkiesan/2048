4c520dae28a0fff235a1c22521130fb04c42e42c
Julien Bouland <bouland.julien@gmail.com>
2020-09-30 18:45:09 +0200

    Added number function

--- src/App.js
+++ src/App.js
@@ -12,6 +12,34 @@ function App() {
 
   // Add number
 
+  const addNumber = (newGrid) => {
+    let added = false;
+    let gridFull = false;
+    let attempts = 0;
+    while (!added) {
+      if (gridFull) {
+        break;
+      }
+
+      let rand1 = Math.floor(Math.random() * 4);
+      let rand2 = Math.floor(Math.random() * 4);
+      attempts++;
+      if (newGrid[rand1][rand2] === 0) {
+        newGrid[rand1][rand2] = Math.random() > 0.5 ? 2 : 4;
+        added = true;
+      }
+    //   if (attempts > 50) {
+    //     gridFull = true;
+    //     let gameOverr = checkIfGameOver();
+    //     if (gameOverr) {
+    //       alert("game over");
+    //       // setGameOver(true);
+    //     }
+    //     // setGameOver(true);
+    //   }
+    // }
+  };
+
   // Swipe left up right down
 
   // Check Gameover
