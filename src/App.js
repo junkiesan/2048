b22168513cfbe4689a8261e8b2192ac675e2fdec
Julien Bouland <bouland.julien@gmail.com>
2020-09-30 19:01:11 +0200

    Added swipe up

--- src/App.js
+++ src/App.js
@@ -150,7 +150,50 @@ function App() {
     }
   };
   // Swipe up
-
+  
+  const swipeUp = (dummy) => {
+    console.log("swipe up");
+    let b = cloneDeep(data);
+    let oldData = JSON.parse(JSON.stringify(data));
+    for (let i = 0; i < 4; i++) {
+      let slow = 0;
+      let fast = 1;
+      while (slow < 4) {
+        if (fast === 4) {
+          fast = slow + 1;
+          slow++;
+          continue;
+        }
+        if (b[slow][i] === 0 && b[fast][i] === 0) {
+          fast++;
+        } else if (b[slow][i] === 0 && b[fast][i] !== 0) {
+          b[slow][i] = b[fast][i];
+          b[fast][i] = 0;
+          fast++;
+        } else if (b[slow][i] !== 0 && b[fast][i] === 0) {
+          fast++;
+        } else if (b[slow][i] !== 0 && b[fast][i] !== 0) {
+          if (b[slow][i] === b[fast][i]) {
+            b[slow][i] = b[slow][i] + b[fast][i];
+            b[fast][i] = 0;
+            fast = slow + 1;
+            slow++;
+          } else {
+            slow++;
+            fast = slow + 1;
+          }
+        }
+      }
+    }
+    if (JSON.stringify(oldData) !== JSON.stringify(b)) {
+      addNumber(b);
+    }
+    if (dummy) {
+      return b;
+    } else {
+      setData(b);
+    }
+  };
   // Swipe down
 
   const swipeDown = (dummy) => {
