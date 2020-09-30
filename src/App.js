b3bc23175bc8a72a86d159166ac098605e47630f
Julien Bouland <bouland.julien@gmail.com>
2020-09-30 18:59:45 +0200

    Added swipe left

--- src/App.js
+++ src/App.js
@@ -54,7 +54,59 @@ function App() {
     }
   };
 
-  // Swipe left up right down
+  // Swipe left 
+
+  const swipeLeft = (dummy) => {
+    console.log("swipe left");
+    let oldGrid = data;
+    let newArray = cloneDeep(data);
+
+    for (let i = 0; i < 4; i++) {
+      let b = newArray[i];
+      let slow = 0;
+      let fast = 1;
+      while (slow < 4) {
+        if (fast === 4) {
+          fast = slow + 1;
+          slow++;
+          continue;
+        }
+        if (b[slow] === 0 && b[fast] === 0) {
+          fast++;
+        } else if (b[slow] === 0 && b[fast] !== 0) {
+          b[slow] = b[fast];
+          b[fast] = 0;
+          fast++;
+        } else if (b[slow] !== 0 && b[fast] === 0) {
+          fast++;
+        } else if (b[slow] !== 0 && b[fast] !== 0) {
+          if (b[slow] === b[fast]) {
+            b[slow] = b[slow] + b[fast];
+            b[fast] = 0;
+            fast = slow + 1;
+            slow++;
+          } else {
+            slow++;
+            fast = slow + 1;
+          }
+        }
+      }
+    }
+    if (JSON.stringify(oldGrid) !== JSON.stringify(newArray)) {
+      addNumber(newArray);
+    }
+    if (dummy) {
+      return newArray;
+    } else {
+      setData(newArray);
+    }
+  };
+  
+  // Swipe right
+
+  // Swipe up
+
+  // Swipe down
 
   // Check Gameover
 
